76b88ede29474ea8b87a130f3bc91fccba63d7ef
VolodymyrVi <sqlphpjs@gmail.com>
2021-06-09 18:38:46 +0300

    fixed

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,10 @@
 
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'"
+      >
         <div class="location-box">
-          <div class="location">Zhytomyr, UA</div>
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
           <div class="date">Monday 09 Juny 2021</div>
         </div>
 
@@ -37,15 +38,17 @@ export default {
     };
   },
   methods:{
-     fetchWeather () {
-       if (e.key == "Enter") {
-         fetch('${this.api_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}')
+     fetchWeather (e) {
+      if (e.key == "Enter") {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
           .then(res => {
             return res.json();
           }).then(this.setResults);
-       }
-     },
-     
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+    }
   }
 };
 </script>
